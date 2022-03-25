8f449839136bcd77fad6b98619be0553bce368df
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2022-03-25 16:48:27 +0100

    p

--- system/mod/moonbreaker.py
+++ system/mod/moonbreaker.py
@@ -1,6 +1,5 @@
 '''
 --|~|--|~|--|~|--|~|--|~|--|~|--
-
 ██  ████        ██████        ██
 ████    ██     ██           ████
 ██      ██   ████████     ██  ██
@@ -8,32 +7,27 @@
 ██             ██       █████████
 ██             ██             ██
 ██
-.codé en : UTF-8
-.langage : python 3
+ - langage : python 3
+ - GitHub  : github.com/pf4-DEV
+ - Licence : GPL-3.0
 --|~|--|~|--|~|--|~|--|~|--|~|--
 '''
 
-version = "0.3b"
+version = "0.5"
 
-def BtoI(bina):
-    return int(bina,2)
-
-def TtoB(text):
-    return(''.join(format(ord(i), '08b') for i in text))
+BtoI = lambda bina: int(bina, 2)
+TtoB = lambda text: "".join(format(ord(i), "08b") for i in text)
 
 def MakeKey(bina,key):
     basse = 1
     sup = BtoI(bina)
-    for x in range(2,len(bina)*key + 20):
-        basse *= int(bina[x%(len(bina))])+1
+    for x in range(2, len(bina) * key + 20):
+        basse *= int(bina[x % (len(bina))]) + 1
 
     while True:
-        if sup > basse:
-            sup /= 2
-        elif sup*10 < basse:
-            sup *= 2
-        else:
-            break
+        if sup > basse: sup /= 2
+        elif sup * 10 < basse: sup *= 2
+        else: break
     
     basse -= sup
     
@@ -46,7 +40,7 @@ def MakeKey(bina,key):
 
 
 def moonbreaker(txt, key = 2):
-    return MakeKey(TtoB(txt)+"1",key)
+    return MakeKey(f"{TtoB(txt)}1", key)
 
 if __name__ == "__main__":
-    while True: print(moonbreaker(input("Texte (str) : "), int(input("Key   (int) : "))))
+    print(moonbreaker(input("Texte (str) : "), int(input("Key   (int) : "))))
